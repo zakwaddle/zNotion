0ce1097eb91e8a02a89461119c7dc40a0bcf7253
Zak Waddle <zakwaddle@gmail.com>
2025-05-24 18:19:24 -0400

    Bump version to 2.0.5
    Update setup.py to reflect the new version number. This ensures package versioning remains consistent and prepares for the next release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zNotion',
-    version='2.0.4',
+    version='2.0.5',
     url='https://github.com/zakwaddle/zNotion',
     packages=find_packages(where="."),
     package_dir={"": "."},
